4cfd8aea1a9f7b5b3cf91e852a6aac8b828dbe63
frans <frans123@live.se>
2025-10-01 13:08:18 +0200

    skapa DiaryEntry-klass med datum, text och ToString

--- dagboksappen/DiaryEntry.cs
+++ dagboksappen/DiaryEntry.cs
@@ -1,1 +1,12 @@
-﻿
+﻿using System;
+
+public class DiaryEntry
+{
+    public DateTime Date { get; set; }
+    public string Text { get; set; }
+
+    public override string ToString()
+    {
+        return $"{Date:yyyy-MM-dd}: {Text}";
+    }
+}
