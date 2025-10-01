f1d9e964f7fb9021fce5dd55984dea047d0da4b3
frans <frans123@live.se>
2025-10-01 12:30:42 +0200

    Add project files.

---
+++ dagboksappen/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace dagboksappen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
